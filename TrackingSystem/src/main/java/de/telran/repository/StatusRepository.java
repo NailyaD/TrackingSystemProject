1bf2b3f31e23060509a02d7a0aa2dce1e2809388
Your NameNailyaD <nailya.doskanova@outlook.com>
2020-03-18 16:40:02 +0100

    Add the GetMapping for all last statuses of all shipments of a customer

--- TrackingSystem/src/main/java/de/telran/repository/StatusRepository.java
+++ TrackingSystem/src/main/java/de/telran/repository/StatusRepository.java
@@ -1,5 +1,6 @@
 package de.telran.repository;
 
+import de.telran.dto.ShipmentsOfCustomerDTO;
 import de.telran.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,9 @@ public interface StatusRepository extends JpaRepository<Status, Long> {
     @Transactional
     @Query("select s from Status s where s.shipmentId = :shipmentId order by s.trackingId desc")
     List<Status> getAllStatusesOfAShipment(@Param("shipmentId") long shipmentId);
+
+    @Query("select new de.telran.dto.ShipmentsOfCustomerDTO (s.customerId, t.trackingId, t.statusTitle, s.shipmentId, s.shipmentTitle) " +
+            "from Status as t join Shipment as s on t.shipmentId = s.shipmentId where s.customerId = :customerId " +
+            "order by t.trackingId desc")
+    List<ShipmentsOfCustomerDTO> getStatusesOfAllShipmentsOfACustomer(@Param("customerId") long customerId);
 }
